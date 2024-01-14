75736b918df153a8a63c391f02a306093d22975d
pwbh <127856937+pwbh@users.noreply.github.com>
2024-01-14 14:30:02 +0200

    minimum working example

--- src/main.cpp
+++ src/main.cpp
@@ -1,36 +1,57 @@
+#include <iostream>
 #define GL_SILENCE_DEPRECATION
+
+#include <glad/glad.h>
 
 #include <GLFW/glfw3.h>
 
+void framebuffer_size_callback(GLFWwindow *window, int width, int height);
+
+void processInput(GLFWwindow *window) {
+  if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+    glfwSetWindowShouldClose(window, true);
+}
+
 int main(void) {
-  GLFWwindow *window;
+  glfwInit();
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+  // glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
-  /* Initialize the library */
-  if (!glfwInit())
-    return -1;
+  GLFWwindow *window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
 
-  /* Create a windowed mode window and its OpenGL context */
-  window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-  if (!window) {
+  if (window == NULL) {
+    std::cout << "Failed to create GLFW window\n ";
     glfwTerminate();
     return -1;
   }
 
-  /* Make the window's context current */
   glfwMakeContextCurrent(window);
 
-  /* Loop until the user closes the window */
+  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+    std::cout << "Failed to initialize GLAD\n ";
+    return -1;
+  }
+
+  glViewport(0, 0, 800, 600);
+
   while (!glfwWindowShouldClose(window)) {
-    /* Render here */
+    // Process input
+    processInput(window);
+    // Render
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
-
-    /* Swap front and back buffers */
+    // Check and call events and swap the buffers
     glfwSwapBuffers(window);
-
-    /* Poll for and process events */
     glfwPollEvents();
   }
 
   glfwTerminate();
+
   return 0;
+}
+
+void framebuffer_size_callback(GLFWwindow *window, int width, int height) {
+  glViewport(0, 0, width, height);
 }
