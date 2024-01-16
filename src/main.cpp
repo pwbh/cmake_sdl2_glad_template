15a78783b421d2c69809c67a2f978214e49e095a
pwbh <127856937+pwbh@users.noreply.github.com>
2024-01-16 17:01:32 +0200

    sdl2 in working state

--- src/main.cpp
+++ src/main.cpp
@@ -3,59 +3,80 @@
 
 #include <glad/gl.h>
 
-#include <GLFW/glfw3.h>
+#include <SDL2/SDL.h>
 
-void framebuffer_size_callback(GLFWwindow *window, int width, int height);
+void handleWindowResize(SDL_Window *window) {
+  int w{};
+  int h{};
+  SDL_GetWindowSize(window, &w, &h);
+  glViewport(0, 0, w, h);
+}
 
-void processInput(GLFWwindow *window) {
-  if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-    glfwSetWindowShouldClose(window, true);
+void handleInput(SDL_Window *window, SDL_Event &event, bool &quit) {
+  while (SDL_PollEvent(&event)) {
+    switch (event.type) {
+    case SDL_QUIT:
+      quit = true;
+      break;
+    case SDL_WINDOWEVENT_RESIZED:
+    case SDL_WINDOWEVENT_SIZE_CHANGED:
+      handleWindowResize(window);
+      break;
+    }
+  }
 }
 
 int main(void) {
-  glfwInit();
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-  // glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+  if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+    std::cout << "Failed to initialize the SDL2 library\n";
+    return -1;
+  }
 
-  GLFWwindow *window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+  SDL_GL_LoadLibrary(NULL);
 
-  if (window == NULL) {
+  SDL_Window *window{SDL_CreateWindow("SDL2 Window", SDL_WINDOWPOS_CENTERED,
+                                      SDL_WINDOWPOS_CENTERED, 1600, 1200, 0)};
+
+  SDL_SetWindowResizable(window, SDL_TRUE);
+
+  if (!window) {
     std::cout << "Failed to create GLFW window\n ";
-    glfwTerminate();
     return -1;
   }
 
-  glfwMakeContextCurrent(window);
+  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
+  SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
 
-  int version = gladLoadGL(glfwGetProcAddress);
+  SDL_GLContext maincontext{SDL_GL_CreateContext(window)};
 
-  if (version == 0) {
-    printf("Failed to initialize OpenGL context\n");
+  if (maincontext == NULL) {
+    std::cout << "Failed to create SDL2 context\n ";
     return -1;
   }
 
-  glViewport(0, 0, 800, 600);
+  // Check OpenGL properties
+  printf("OpenGL loaded\n");
 
-  glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+  gladLoadGL((GLADloadfunc)SDL_GL_GetProcAddress);
 
-  while (!glfwWindowShouldClose(window)) {
-    // Process input
-    processInput(window);
-    // Render
-    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+  printf("Vendor:   %s\n", glGetString(GL_VENDOR));
+  printf("Renderer: %s\n", glGetString(GL_RENDERER));
+  printf("Version:  %s\n", glGetString(GL_VERSION));
+
+  handleWindowResize(window);
+
+  SDL_Event event;
+
+  bool quit{false};
+
+  while (!quit) {
+    glClearColor(0.2f, 0.3f, 1.f, 0.f);
     glClear(GL_COLOR_BUFFER_BIT);
-    // Check and call events and swap the buffers
-    glfwSwapBuffers(window);
-    glfwPollEvents();
-  }
 
-  glfwTerminate();
+    SDL_GL_SwapWindow(window);
+    handleInput(window, event, quit);
+  }
 
   return 0;
-}
-
-void framebuffer_size_callback(GLFWwindow *window, int width, int height) {
-  glViewport(0, 0, width, height);
 }
